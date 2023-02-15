ddaf1bc0ef2b7019cef89f36046bbd656e931f31
FilipeFlip <filipe_supra1998@hotmail.com>
2023-02-15 19:27:57 -0300

    comentarios/documentação FrontEnd

--- FrontEnd/main.py
+++ FrontEnd/main.py
@@ -16,6 +16,22 @@ from Tela_Historico import *
 
 class Ui_Main(QtWidgets.QWidget):
     def setupUi(self, Main):
+        '''
+        Esta classe define a interface gráfica do usuário usando PyQt5. 
+        Ele define uma janela principal (Main) e um layout de pilha de várias outras janelas (stack0 a stack7) 
+        que podem ser alternadas dentro da janelaprincipal. 
+        Cada tela é definida em seu próprio arquivo de classe, como TelaCadastro, TelaLogin etc. 
+        O método setupUi() é chamado para cada tela para configurar sua aparência e funcionamento, 
+        e cada uma delas é adicionada ao layout de pilha por meio do método addWidget().
+
+        Parameters
+        ----------
+        Main: Object
+
+        Returns
+        -------
+        None
+        '''
         Main.setObjectName('Main')
         Main.resize(640, 480)
 
@@ -69,14 +85,22 @@ class Main(QMainWindow, Ui_Main):
         super(Main, self).__init__(None)
         self.setupUi(self)
 
-        
+        '''
+        Esse trecho de código tenta se conectar a um servidor na máquina com o endereço IP '10.0.0.102' e a porta 8005. Se a conexão for recusada, 
+        uma caixa de mensagem será exibida informando que não foi possível se conectar ao servidor e a aplicação será encerrada usando sys.exit().
+        '''
         try:
             self.server = server_cliente('10.0.0.102', 8005)
         except ConnectionRefusedError:
             QtWidgets.QMessageBox.information(None, 'ERROR', f'Não foi possível conectar ao servidor.'
-                                                             f'\nVerifique a conexão e tente novamente')
+                                                            f'\nVerifique a conexão e tente novamente')
             sys.exit()
         
+        '''
+        Este trecho de código define as conexões de sinal e slot para vários botões da interface do usuário. 
+        Cada vez que um botão é clicado, ele chama a função correspondente que executa uma determinada ação. 
+        Por exemplo, ao clicar no botão 'ButtonLogin' em 'Tela_Login', o método 'BotaoLogin' é chamado.
+        '''
         self.index = None
 
         self.Tela_Inicial.ButtonCadastrar.clicked.connect(self.BotaoParaTelaCadastro)
@@ -109,52 +133,192 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Historico.ButtonVoltar.clicked.connect(self.BotaoParaTelaPrincipal)
 
     def sair(self):
+        '''
+        Botão para sair do sistema.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.request_server('sair')
         sys.exit()
 
     def BotaoVoltarTelaInicial(self):
+        '''
+        Botão que volta para a tela inicial do sistema.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.QtStack.setCurrentIndex(0)
         self.Tela_Cadastro.labelNotificacao.setText('')
         self.Tela_Login.labelNotificacao.setText('')
 
     def BotaoParaTelaCadastro(self):
+        '''
+        Botão para abrir a tela de cadastro.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.QtStack.setCurrentIndex(1)
 
     def BotaoParaTelaLogin(self):
+        '''
+        Botão para abrir a tela de login.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.QtStack.setCurrentIndex(2)
 
     def BotaoParaTelaPrincipal(self):
+        '''
+        Botão para a tela do menu do usuario. 
+        ele limpa o campo de notificação da tela de principal, em seguida altera a tela atual do sistema para a tela principal.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.QtStack.setCurrentIndex(3)
         self.Tela_Deposito.labelNotificacao.setText('')
         self.Tela_Saque.labelNotificacao.setText('')
 
     def BotaoParaTelaDeposito(self):
+        '''
+        Botão para a tela de deposito, 
+        ele limpa o campo de notificação da tela de deposito, em seguida altera a tela atual do sistema para a tela de deposito.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.Tela_Deposito.labelNotificacao.setText('')
         self.QtStack.setCurrentIndex(4)
 
     def BotaoParaTelaSacar(self):
+        '''
+        Botão para a tela sacar, 
+        ele limpa o campo de notificação da tela de saque, em seguida altera a tela atual do sistema para a tela de deposito.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.Tela_Saque.labelNotificacao.setText('')
         self.QtStack.setCurrentIndex(5)
 
     def BotaoParaTelaTransferir(self):
+        '''
+        Botão para a tela transferir, 
+        ele limpa o campo de notificação da tela transferir, em seguida altera a tela atual do sistema para a tela de transferir.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         self.Tela_Transferir.labelNotificacao.setText('')
         self.QtStack.setCurrentIndex(6)
     
     def request_server(self, request):
+        '''
+        Essa função envia uma requisição de mensagem para o servidor com a request como parâmetro. 
+        Em seguida, aguarda uma resposta de até 2048 bytes do servidor e a converte em uma string. 
+        Então, remove todos os caracteres desnecessários da resposta e retorna como uma lista de palavras.
+
+        Parameters
+        ----------
+        request :Str
+
+        Returns
+        -------
+        flag
+        '''
         self.server.send(request.encode())
         recv = self.server.recv(2048)
         flag = recv.decode()
-        # print(flag, type(flag))
         flag = flag.replace("(", "").replace(")", "").replace("[", "").replace("]", "").replace(",", "").replace("'", '').split()
         return flag
     
+
     def concatenar(self, string):
+        '''
+        Este é um método chamado concatenar que recebe uma string como entrada e concatena todos os caracteres da string, 
+        exceto o primeiro caractere (índice 0), com um caractere de espaço entre cada caractere.
+        A string concatenada resultante é então retornada.
+
+        Parameters
+        ----------
+        string: Str
+
+        Returns
+        -------
+        noti
+        '''
         noti = ''
         for i in range(1,len(string)):
             noti += string[i] + " "
         return noti
 
     def BotaoCadastrar(self):
+        '''
+        Este é um método chamado BotaoCadastrar que é acionado quando o botão "Cadastrar" é clicado na tela de cadastro. 
+        Ele recupera os valores dos campos de entrada para nome, cpf, 
+        usuário e senha da tela de cadastro e verifica se todos os campos estão preenchidos. 
+        Se algum campo estiver vazio, uma notificação será exibida na tela.
+
+        Se todos os campos estiverem preenchidos, o código verifica se o CPF tem 11 dígitos e se é composto apenas por números. 
+        Em seguida, a senha é codificada em utf-8 e criptografada usando o algoritmo MD5. 
+        Uma solicitação é enviada ao servidor contendo as informações do novo usuário a ser adicionado. 
+        A resposta do servidor é concatenada em uma string e exibida na tela de cadastro.
+
+        Independentemente de ter sucesso ou falhar, todos os campos da tela de cadastro são limpos para que um novo usuário possa ser adicionado. 
+        Se o CPF não atender aos parâmetros necessários, uma notificação informando isso é exibida na tela
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         nome = self.Tela_Cadastro.lineNome.text()
         cpf = self.Tela_Cadastro.lineCpf.text()
         usuario = self.Tela_Cadastro.lineUsuario.text()
@@ -181,6 +345,27 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Cadastro.lineSenha.setText("")
 
     def BotaoLogin(self):
+        '''
+        Este é um método chamado BotaoLogin que é acionado quando o botão "Login" é clicado na tela de login. 
+        Ele recupera os valores dos campos de entrada para usuário e senha da tela de login e verifica se ambos estão preenchidos. 
+        Se algum campo estiver vazio, uma notificação será exibida na tela.
+
+        Se ambos os campos estiverem preenchidos, o código codifica a senha em utf-8 e a criptografa usando o algoritmo MD5. 
+        Em seguida, é criada uma solicitação contendo as informações de login do usuário e enviada para o servidor. 
+        A resposta do servidor é recebida e processada. 
+        Se o primeiro elemento da resposta for 'True', o login é bem-sucedido e o usuário é direcionado para a tela principal do sistema.
+
+        Caso contrário, uma notificação informando que o login falhou é exibida na tela de login. 
+        Independentemente de ter sucesso ou falhar, os campos da tela de login são limpos para que um novo login possa ser feito.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         usuario = self.Tela_Login.lineUsuario.text()
         senha = self.Tela_Login.lineSenha.text()
         if usuario != '' and senha != '':
@@ -196,9 +381,6 @@ class Main(QMainWindow, Ui_Main):
             if flag4[0] == 'True':
                 
                 self.numero = int(flag4[3])
-                # self.Tela_Principal.labelNome.setText(f"Olá, {flag4[1][1][0]}")
-                # self.Tela_Principal.labelSaldo.setText(f"R$ {float(flag4[2]):.2f}")
-                # self.Tela_Principal.labelNumero.setText(f"Conta {int(flag4[3])}")
                 self.Tela_Principal.labelNome.setText(f"Olá, {flag4[1]}")
                 self.Tela_Principal.labelSaldo.setText(f"R$ {float(flag4[2]):.2f}")
                 self.Tela_Principal.labelNumero.setText(f"Conta {int(flag4[3])}")
@@ -213,11 +395,42 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Login.lineSenha.setText("")
 
     def BotaoAtualizar(self):
+        '''
+        Este é um método chamado BotaoAtualizar que é acionado quando o botão "Atualizar" é clicado na tela principal do sistema. 
+        Ele cria uma solicitação contendo o número da conta do usuário e o tipo de solicitação, que é "verificarSaldo", e envia essa solicitação para o servidor.
+
+        Em seguida, ele recebe uma resposta do servidor contendo o saldo atualizado da conta. 
+        O saldo é formatado como um número de ponto flutuante com duas casas decimais e exibido na tela principal do sistema, 
+        na label denominada "labelSaldo".
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         solicit = f'verificarSaldo*{self.numero}'
         flag = self.request_server(solicit)
         self.Tela_Principal.labelSaldo.setText(f"R$ {float(flag[0]):.2f}")
 
     def BotaoDepoistar(self):
+        '''
+        Este é um método chamado BotaoDepoistar que é acionado quando o botão "Depositar" é clicado na tela de depósito. 
+        Ele verifica se o campo de valor não está vazio e se contém apenas dígitos. 
+        Se for o caso, uma solicitação de depósito é enviada ao servidor com o número da conta e o valor do depósito. 
+        O campo de notificação da tela de depósito é atualizado com a mensagem retornada pelo servidor e, em seguida, o campo de valor é limpo. 
+        Se o campo de valor estiver vazio ou não contiver apenas dígitos, uma mensagem apropriada é exibida no campo de notificação.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         valor = self.Tela_Deposito.lineSaldo.text()
         if valor != '':
             if valor.replace('.','').isdigit():
@@ -232,6 +445,21 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Deposito.lineSaldo.setText('')
 
     def BotaoSacar(self):
+        '''
+        O método BotaoSacar é chamado quando o usuário clica no botão "Sacar" na tela de operações. 
+        Ele pega o valor e a senha inseridos pelo usuário, verifica se o valor é um número válido e codifica a senha usando o algoritmo MD5. 
+        Em seguida, constrói uma mensagem com o número da conta, o valor e a senha codificada e envia uma solicitação ao servidor. 
+        Se a operação for bem-sucedida, uma mensagem de notificação é exibida na tela de operações, caso contrário, 
+        uma mensagem de senha inválida é exibida. Por fim, os campos de valor e senha são limpos.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         valor = self.Tela_Saque.lineSaldo.text()
         senha = self.Tela_Saque.lineSenha.text()
         if valor != '' and senha != '':
@@ -253,6 +481,28 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Saque.lineSenha.setText('')
 
     def BotaoTransferir(self):
+        '''
+        Este é um método que é acionado quando o botão "Transferir" é pressionado na tela. 
+        Ele pega o valor que foi inserido no campo de valor (lineSaldo), 
+        a senha (lineSenha) e o número da conta de destino (lineNumero) da tela de transferência e verifica se todos eles foram preenchidos. 
+        Se estiverem preenchidos, ele verifica se os campos de valor e número da conta contêm apenas números. 
+        Se ambos forem numéricos, a senha é codificada em UTF-8 e convertida em um hash md5. Em seguida, 
+        é criada uma solicitação de transferência com o número da conta de origem (self.numero), a senha hash, 
+        o número da conta de destino e o valor a ser transferido. 
+        Essa solicitação é enviada para o servidor por meio do método request_server() e a resposta é armazenada na variável flag. 
+        Em seguida, a notificação de transferência é exibida na tela de transferência com a mensagem da resposta do servidor. 
+        Caso contrário, a notificação informa que apenas números devem ser inseridos nos campos de valor e número da conta, 
+        ou que todos os campos devem ser preenchidos. 
+        Em todos os casos, os campos de valor, senha e número da conta são redefinidos.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         valor = self.Tela_Transferir.lineSaldo.text()
         senha = self.Tela_Transferir.lineSenha.text()
         numero = self.Tela_Transferir.lineNumero.text()
@@ -274,6 +524,22 @@ class Main(QMainWindow, Ui_Main):
         self.Tela_Transferir.lineNumero.setText('')
     
     def concatenarHis(self, string):
+        '''
+        Essa função recebe uma string e a transforma em um formato mais legível para o usuário exibir na tela como uma notificação de histórico. 
+        Primeiramente, a função cria uma variável noti que vai armazenar a string processada. 
+        Em seguida, ela percorre a string, adiciona um espaço entre cada elemento e armazena em noti. 
+        Depois, ela quebra a string em uma lista com o separador '\n' e adiciona cada elemento em uma nova string a, 
+        adicionando uma quebra de linha para cada elemento. 
+        Por fim, a função retorna a string a.
+
+        Parameters
+        ----------
+        string: str
+
+        Returns
+        -------
+        a
+        '''
         noti = ''
         for i in range(len(string)):
             noti += string[i] + ' '
@@ -284,6 +550,20 @@ class Main(QMainWindow, Ui_Main):
         return a
 
     def BotaoParaTelaHistorico(self):
+        '''
+        A função BotaoParaTelaHistorico é responsável por obter o histórico de transações bancárias do cliente e exibi-las na tela de histórico.
+        Para isso, é feita uma solicitação ao servidor bancário com o número da conta do cliente e o tipo de solicitação, que é "historico". 
+        Em seguida, o resultado é passado para a função concatenarHis que formata as informações em uma string para ser exibida na tela.
+        Por fim, a string formatada é atribuída ao widget de texto textHistorico da tela de histórico e a tela de histórico é exibida.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         solicit = f'historico*{self.numero}'
         flag = self.request_server(solicit)
         noti = self.concatenarHis(flag)
@@ -291,11 +571,30 @@ class Main(QMainWindow, Ui_Main):
         self.QtStack.setCurrentIndex(7)
 
     def BotaoExcluir(self):
+        '''
+        Essa função BotaoExcluir é responsável por enviar uma solicitação ao servidor para excluir a conta do usuário logado. 
+        Ela constrói a solicitação concatenando o número da conta do usuário com o comando "excluirConta" e então chama a função request_server para enviar a solicitação ao servidor. 
+        Depois de enviar a solicitação, a função muda a página atual da GUI para a página inicial (index 0 na pilha de widgets QtStack).
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        '''
         solicit = f'excluirConta*{self.numero}'
         self.request_server(solicit)
         self.QtStack.setCurrentIndex(0)
 
 if __name__ == "__main__":
+    '''
+    Esse trecho de código é o ponto de entrada do programa. 
+    Ele verifica se o módulo atual é o módulo principal (ou seja, não foi importado como um módulo em outro arquivo) e, se for, 
+    cria uma instância do aplicativo QApplication e a janela principal Main, e inicia o loop de eventos do aplicativo chamando app.exec_(). 
+    Quando a janela principal é fechada, o loop de eventos termina e a execução do programa é encerrada
+    '''
     app = QtWidgets.QApplication(sys.argv)
     show_main = Main()
     sys.exit(app.exec_())
--- FrontEnd/server_cliente.py
+++ FrontEnd/server_cliente.py
@@ -1,20 +1,28 @@
 import socket 
 
 def server_cliente(ip, port):
-    ip = ip             # '192.168.186.160' 
+    '''
+    A função server_cliente é responsável por criar e retornar um objeto de socket para o cliente
+    se conectar a um servidor com o endereço IP e a porta especificados. 
+    A função recebe dois parâmetros: ip e port, que são respectivamente o endereço IP e a porta do servidor. 
+    O endereço é definido pela tupla (ip, port). É criado um objeto de socket client_socket com o tipo de endereço IPv4
+    e o protocolo de transporte TCP. 
+    Em seguida, o cliente se conecta ao endereço especificado com o método connect(). 
+    Por fim, o objeto de socket é retornado.
+
+    Parameters
+        ----------
+        ip: Str
+
+        port: Str
+
+        Returns
+        -------
+        client_socket
+    '''
+    ip = ip
     port = port
     addr = (ip, port) 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     client_socket.connect(addr)
     return client_socket
-        
-
-    # def solicitacao(self, solicit):
-    #     print(solicit)
-    #     self.client_socket.connect(self.addr)
-    #     self.client_socket.send(solicit.encode()) #envia a mensagem
-    #     print("Mensagem enviada.")
-    #     print(self.client_socket.recv(2048).decode()) #)
-    
-    # def close(self): 
-    #     self.client_socket.close()
